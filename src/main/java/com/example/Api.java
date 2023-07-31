dabcd9611a41e7a4cbcd4c16de49b1ee0934bca6
Roar Skinderviken <roar.skinderviken@gmail.com>
2023-07-31 17:30:55 +0200

    Moved Api.java to Java-folder. Added BrokenControllerTest. Commented out non-related @RequestAttribute for now. Removed redundant application.yaml.

--- src/main/java/com/example/Api.java
+++ src/main/java/com/example/Api.java
@@ -3,7 +3,6 @@ package com.example;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.annotation.QueryValue;
-import io.micronaut.http.annotation.RequestAttribute;
 
 public interface Api {
 
@@ -11,5 +10,5 @@ public interface Api {
     String foo(@QueryValue(value = "query") String query);
 
     @Post(uri = "/broken/bar", produces = {"text/plain"})
-    String bar(@QueryValue(value = "query") String query, @RequestAttribute("a-request-attribute") String attribute);
+    String bar(@QueryValue(value = "query") String query/*, @RequestAttribute("a-request-attribute") String attribute*/);
 }
